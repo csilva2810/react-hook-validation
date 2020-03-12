687a4022a8c46fc3cfd3f80d802b5a56d5270508
Carlos Silva <csilva2810@gmail.com>
2020-03-12 07:53:07 -0300

    step2 - implements the required rule

--- src/useForm.js
+++ src/useForm.js
@@ -7,7 +7,25 @@ export function useForm({ validations }) {
     throw new Error('the option `validations` should be an object');
   }
 
-  function validateField(name, value) {}
+  function validateField(name, value) {
+    // get the validation rules for the field
+    const rules = validations[name];
+
+    // check if the rules exist, since a field can not have validations
+    if (rules) {
+      // if the required rule is registered
+      if (rules.required) {
+        // now we validate the value checking if it has a value
+        // we are using trim, to strip whitespaces before and after the value
+        if (!value.trim()) {
+          return 'required';
+        }
+      }
+    }
+
+    // if there are no erros, we return an empty string
+    return '';
+  }
 
   return {
     validateField,
